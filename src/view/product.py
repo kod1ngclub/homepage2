8d48e63459d3136b50e6aca6c430627b7033d451
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-10 07:16:41 +0900

    FIX: Remove unnecessary dataclass deco at Enum

--- src/view/product.py
+++ src/view/product.py
@@ -1,26 +1,23 @@
 from dataclasses import dataclass
 from enum import Enum
 
-@dataclass
 class ProductCard:
     head: str
     body: str
     star: bool
 
 class LevelFiltered(Enum):
-    NotFiltered     = 0
+    NonFiltered     = 0
     Major           = 1
     Toy             = 2
     Deprecated      = 3
 
-@dataclass
 class SeriesFiltered(Enum):
     NonFiltered     = 0
     Host            = 1
     Cod             = 2
     Etc             = 3
 
-@dataclass
 class StarFiltered(Enum):
     NonFiltered     = 0
     Star            = 1
