c8104ae18446b3d96259e7e4e1541bb8ad05f91f
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-12 18:02:26 +0900

    FEAT: Build func - build according to data

--- src/main.py
+++ src/main.py
@@ -34,65 +34,69 @@ from template.interface.datapage import OutItem
 from template.interface.engine import TemplateEngine
 from template.test import TestTemplateEngine
 
-# ==== data-fetching
-landingdata: LandingData        = LandingData(products=[], serieses=[], github="", discord="")
-productdata: ProductData        = ProductData(products=[])
-contactdata: ContactData        = ContactData(contacts=[])
-donatedata: DonateData          = DonateData(donates=[])
 
-# ==== build pages
-landingserv: LandingService     = LandingService(data=landingdata)
-productserv: ProductService     = ProductService(data=productdata)
-contactserv: ContactService     = ContactService(data=contactdata)
-donateserv: DonateService       = DonateService(data=donatedata)
+def Build(landingdata: LandingData, productdata: ProductData, contactdata: ContactData, donatedata: DonateData):
+    # Init service
+    landingserv: LandingService         = LandingService(data=landingdata)
+    productserv: ProductService         = ProductService(data=productdata)
+    contactserv: ContactService         = ContactService(data=contactdata)
+    donateserv: DonateService           = DonateService(data=donatedata)
 
-landingpage: LandingPage        = landingserv.Build()
-productindex: ProductPage       = productserv.Build()
-productgroup: list[ProductPage] = productserv.BuildEach()
-contactpage: ContactPage        = contactserv.Build()
-donatepage: DonatePage          = donateserv.Build()
+    # Build pages
+    landingpage: LandingPage            = landingserv.Build()
+    productindex: ProductPage           = productserv.Build()
+    productgroup: list[ProductPage]     = productserv.BuildEach()
+    contactpage: ContactPage            = contactserv.Build()
+    donatepage: DonatePage              = donateserv.Build()
 
-# ==== bind datapage
-page: DataPage = DataPage(
-    landing     = LandingGroup(
-        index       = landingpage,
-        group       = None
-    ),
-    product     = ProductGroup(
-        index       = productindex,
-        group       = productgroup
-    ),
-    contact     = ContactGroup(
-        index       = contactpage,
-        group       = None
-    ),
-    donate      = DonateGroup(
-        index       = donatepage,
-        group       = None
-    ),
+    # Bind to datapage
+    page: DataPage = DataPage(
+        landing = LandingGroup(
+            index       = landingpage,
+            group       = None
+        ),
+        product = ProductGroup(
+            index       = productindex,
+            group       = productgroup
+        ),
+        contact = ContactGroup(
+            index       = contactpage,
+            group       = None
+        ),
+        donate = DonateGroup(
+            index       = donatepage,
+            group       = None
+        ),
 
-    nav         = Nav(
-        items=[
-            LocalItem(text="Home", href=LocalHref.Root),
-            LocalItem(text="Product", href=LocalHref.Product),
-            LocalItem(text="Contact", href=LocalHref.Contact),
-            LocalItem(text="Donate", href=LocalHref.Donate)
-        ]
-    ),
-    foot        = Foot(
-        table=[
-            LocalItem(text="Home", href=LocalHref.Root),
-            LocalItem(text="Product", href=LocalHref.Product),
-            LocalItem(text="Contact", href=LocalHref.Contact),
-            LocalItem(text="Donate", href=LocalHref.Donate)
-        ],
-        family=[
-            OutItem(text="Google", href=OutHref(to="https://www.google.com")),
-            OutItem(text="Example", href=OutHref(to="https://www.example.com"))
-        ]
+        nav = Nav(
+            items = [
+                LocalItem(text="Home", href=LocalHref.Root),
+                LocalItem(text="Product", href=LocalHref.Product),
+                LocalItem(text="Contact", href=LocalHref.Contact),
+                LocalItem(text="Donate", href=LocalHref.Donate)
+            ]
+        ),
+        foot = Foot(
+            table = [
+                LocalItem(text="Home", href=LocalHref.Root),
+                LocalItem(text="Product", href=LocalHref.Product),
+                LocalItem(text="Contact", href=LocalHref.Contact),
+                LocalItem(text="Donate", href=LocalHref.Donate)
+            ],
+            family = [
+                OutItem(text="Google", href=OutHref(to="https://www.google.com")),
+                OutItem(text="Example", href=OutHref(to="https://www.example.com"))
+            ]
+        )
     )
-)
 
-# ==== render pages (with engine)
-engine: TemplateEngine  = TestTemplateEngine()
-engine.Run(datapage=page)
+    # Render with engine
+    engine: TemplateEngine  = TestTemplateEngine()
+    engine.Run(datapage=page)
+
+Build(
+    landingdata     = LandingData(products=[], serieses=[], github="", discord=""),
+    productdata     = ProductData(products=[]),
+    contactdata     = ContactData(contacts=[]),
+    donatedata      = DonateData(donates=[])
+)
