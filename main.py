a20a1ba85246f9e1342abdd20ba0c3dd1ce83ffd
kod1ngclub <wiiwefwefbi@gmail.com>
2025-01-16 16:14:17 +0900

    Some changes maybe?

--- main.py
+++ main.py
@@ -37,10 +37,16 @@ from template.interface.datapage import OutItem
 
 from template.interface.engine import TemplateEngine
 from template.interface.engine import TemplateConfig
-
+from template.test import TestTemplateEngine
 from template.jinja import JinjaTemplateEngine
+
 from template.jinja import JinjaConfig
 from template.jinja import SitemapIndexConfig
+from template.jinja import SitemapRoot
+from template.jinja import SitemapSub
+from template.jinja import HeadConfig
+from template.jinja import RemotePNG
+from template.jinja import RemotePNGIcon
 
 # resources
 from resources.test.data import TEST_HEAD
@@ -57,7 +63,7 @@ from resources.test.donate import TEST_DONATES
 # lib
 from pathlib import Path
 
-def Build(landingdata: LandingData, productdata: ProductData, contactdata: ContactData, donatedata: DonateData, engine: TemplateEngine):
+def Build(landingdata: LandingData, productdata: ProductData, contactdata: ContactData, donatedata: DonateData, engine: TemplateEngine, conf: TemplateConfig):
     # Init service
     landingserv: LandingService         = LandingService(data=landingdata)
     productserv: ProductService         = ProductService(data=productdata)
@@ -113,71 +119,93 @@ def Build(landingdata: LandingData, productdata: ProductData, contactdata: Conta
     )
 
     # Render with engine
+    engine.Init(conf)
     engine.Run(datapage=page)
 
-# template filepath words
+# def filepath words
+WORD_BUILD: str             = "build"
 WORD_RESOURCES: str         = "resources"
 WORD_TEMPLATE: str          = "template"
+WORD_STATIC: str            = "static"
 WORD_LAYOUT: str            = "layout.html"
 WORD_LANDING: str           = "landing.html"
 WORD_PRODUCT: str           = "product.html"
 WORD_CONTACT: str           = "contact.html"
 WORD_DONATE: str            = "donate.html"
 
-# define filepaths as 'Path'
-RESOURCES_TEMPLATE: Path                = Path.cwd().joinpath(WORD_RESOURCES).joinpath(WORD_TEMPLATE)
-RESOURCES_TEMPLATE_LAYOUT: Path         = RESOURCES_TEMPLATE.joinpath(WORD_LAYOUT)
-RESOURCES_TEMPLATE_LANDING: Path        = RESOURCES_TEMPLATE.joinpath(WORD_LANDING)
-RESOURCES_TEMPLATE_PRODUCT: Path        = RESOURCES_TEMPLATE.joinpath(WORD_PRODUCT)
-RESOURCES_TEMPLATE_CONTACT: Path        = RESOURCES_TEMPLATE.joinpath(WORD_CONTACT)
-RESOURCES_TEMPLATE_DONATE: Path         = RESOURCES_TEMPLATE.joinpath(WORD_DONATE)
+# def filepath constants
+FILEPATH_BUILD: Path                             = Path.cwd().joinpath(WORD_BUILD)
+FILEPATH_RESOURCES_TEMPLATE: Path                = Path.cwd().joinpath(WORD_RESOURCES).joinpath(WORD_TEMPLATE)
+FILEPATH_RESOURCES_STATIC: Path                  = Path.cwd().joinpath(WORD_RESOURCES).joinpath(WORD_STATIC)
+FILEPATH_RESOURCES_TEMPLATE_LAYOUT: Path         = FILEPATH_RESOURCES_TEMPLATE.joinpath(WORD_LAYOUT)
+FILEPATH_RESOURCES_TEMPLATE_LANDING: Path        = FILEPATH_RESOURCES_TEMPLATE.joinpath(WORD_LANDING)
+FILEPATH_RESOURCES_TEMPLATE_PRODUCT: Path        = FILEPATH_RESOURCES_TEMPLATE.joinpath(WORD_PRODUCT)
+FILEPATH_RESOURCES_TEMPLATE_CONTACT: Path        = FILEPATH_RESOURCES_TEMPLATE.joinpath(WORD_CONTACT)
+FILEPATH_RESOURCES_TEMPLATE_DONATE: Path         = FILEPATH_RESOURCES_TEMPLATE.joinpath(WORD_DONATE)
 
 # check usable
 def FAIL_TO_FIND(path: Path):   return (not path.exists())
-def TO_FILEPATH(path: Path):    return str(path)
+def TO_STR(path: Path):         return str(path)
 
-if FAIL_TO_FIND(RESOURCES_TEMPLATE):            raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE)} not exist")
-if FAIL_TO_FIND(RESOURCES_TEMPLATE_LAYOUT):     raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE_LAYOUT)} not exist")
-if FAIL_TO_FIND(RESOURCES_TEMPLATE_LANDING):    raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE_LANDING)} not exist")
-if FAIL_TO_FIND(RESOURCES_TEMPLATE_PRODUCT):    raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE_PRODUCT)} not exist")
-if FAIL_TO_FIND(RESOURCES_TEMPLATE_CONTACT):    raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE_CONTACT)} not exist")
-if FAIL_TO_FIND(RESOURCES_TEMPLATE_DONATE):     raise Exception(f"Fielpath {TO_FILEPATH(RESOURCES_TEMPLATE_DONATE)} not exist")
-
-engine: TemplateEngine = JinjaTemplateEngine(
-    jinja = JinjaConfig(
-        root        = TO_FILEPATH(RESOURCES_TEMPLATE),
-        landing     = WORD_LANDING,
-        product     = WORD_PRODUCT,
-        contact     = WORD_CONTACT,
-        donate      = WORD_DONATE
-    ),
-    sitemap = SitemapIndexConfig(
-        root        = "https://www.example.com",
-        landing     = None,
-        product     = "product",
-        contact     = "contact",
-        donate      = "donate"
-    )
-)
-engine.Init(
-    TemplateConfig(
-        path        = "/build",
-        debug       = True
-    )
-)
+if FAIL_TO_FIND(FILEPATH_BUILD):                        raise Exception(f"Filepath {TO_STR(FILEPATH_BUILD)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_STATIC):             raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_STATIC)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE):           raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE_LAYOUT):    raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE_LAYOUT)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE_LANDING):   raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE_LANDING)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE_PRODUCT):   raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE_PRODUCT)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE_CONTACT):   raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE_CONTACT)} not exist")
+if FAIL_TO_FIND(FILEPATH_RESOURCES_TEMPLATE_DONATE):    raise Exception(f"Filepath {TO_STR(FILEPATH_RESOURCES_TEMPLATE_DONATE)} not exist")
 
 Build(
     landingdata     = LandingData(
-        head        = TEST_HEAD,
-        body        = TEST_BODY,
-        abouts      = TEST_ABOUTS,
-        products    = TEST_EXAMPLE_PRODUCTS,
-        serieses    = TEST_EXAMPLE_SERIESES,
-        github      = TEST_GITHUB,
-        discord     = TEST_DISCORD
+        head            = TEST_HEAD,
+        body            = TEST_BODY,
+        abouts          = TEST_ABOUTS,
+        products        = TEST_EXAMPLE_PRODUCTS,
+        serieses        = TEST_EXAMPLE_SERIESES,
+        github          = TEST_GITHUB,
+        discord         = TEST_DISCORD
     ),
     productdata     = ProductData(products=TEST_PRODUCTS),
     contactdata     = ContactData(contacts=TEST_CONTACTS),
     donatedata      = DonateData(donates=TEST_DONATES),
-    engine          = engine
+    engine          = JinjaTemplateEngine(
+        jinja = JinjaConfig(
+            root        = TO_STR(FILEPATH_RESOURCES_TEMPLATE),
+            landing     = WORD_LANDING,
+            product     = WORD_PRODUCT,
+            contact     = WORD_CONTACT,
+            donate      = WORD_DONATE
+        ),
+        sitemap = SitemapIndexConfig(
+            root        = "https://www.example.com",
+            landing     = "https://www.example.com",
+            product     = "https://www.example.com/product",
+            contact     = "https://www.example.com/contact",
+            donate      = "https://www.example.com/donate"
+        ),
+        head = HeadConfig(
+            langcode    = "ko",
+
+            icon16      = RemotePNGIcon(href="https://www.example.com/icon16.png", size=16),
+            icon32      = RemotePNGIcon(href="https://www.example.com/icon16.png", size=32),
+            icon64      = RemotePNGIcon(href="https://www.example.com/icon16.png", size=64),
+            icon128     = RemotePNGIcon(href="https://www.example.com/icon16.png", size=128),
+            icon256     = RemotePNGIcon(href="https://www.example.com/icon16.png", size=256),
+
+            author      = "authorname",
+            email       = "someone@example.com",
+
+            opengraph   = RemotePNG(
+                href        = "https://www.example.com/opengraph.png",
+                width       = 1200,
+                height      = 600,
+                alt         = "Opengraph image of website"
+            )
+        )
+    ),
+    conf = TemplateConfig(
+        path    = TO_STR(FILEPATH_BUILD),
+        debug   = True
+    )
 )
--- template/jinja.py
+++ template/jinja.py
@@ -17,6 +17,7 @@ from template.interface.datapage import DataPage
 from jinja2 import Environment
 from jinja2 import FileSystemLoader
 from jinja2 import Template
+from copy import deepcopy
 
 from dataclasses import dataclass
 from dataclasses import asdict
@@ -32,12 +33,46 @@ class JinjaConfig:
     donate: str
 
 @dataclass
+class SitemapRoot:
+    href: str
+
+@dataclass
+class SitemapSub:
+    sub: str | None
+
+@dataclass
 class SitemapIndexConfig:
     root: str
-    landing: str | None
-    product: str | None
-    contact: str | None
-    donate: str | None
+    landing: str
+    product: str
+    contact: str
+    donate: str
+
+@dataclass
+class RemotePNG:
+    href: str
+    width: int
+    height: int
+    alt: str
+
+@dataclass
+class RemotePNGIcon:
+    href: str
+    size: int
+
+@dataclass
+class HeadConfig:
+    langcode: str
+
+    icon16: RemotePNGIcon
+    icon32: RemotePNGIcon
+    icon64: RemotePNGIcon
+    icon128: RemotePNGIcon
+    icon256: RemotePNGIcon
+    opengraph: RemotePNG
+
+    author: str
+    email: str
 
 # ==== Filter to Word map for URL
 class HrefLevelFilter(Enum):
@@ -101,15 +136,42 @@ class DataJSON:
 @dataclass
 class SitemapJSON:
     root: str
-    landing: str | None
-    product: str | None
-    contact: str | None
-    donate: str | None
+    landing: str
+    product: str
+    contact: str
+    donate: str
+
+@dataclass
+class RemotePNGJSON:
+    href: str
+    width: int
+    height: int
+    alt: str
+
+@dataclass
+class RemotePNGIconJSON:
+    href: str
+    size: int
+
+@dataclass
+class HeadJSON:
+    langcode: str
+
+    icon16: RemotePNGIconJSON
+    icon32: RemotePNGIconJSON
+    icon64: RemotePNGIconJSON
+    icon128: RemotePNGIconJSON
+    icon256: RemotePNGIconJSON
+    opengraph: RemotePNGJSON
+
+    author: str
+    email: str
 
 @dataclass
 class JinjaJSON:
     data: DataJSON
     sitemap: SitemapJSON
+    head: HeadJSON
 
 class JinjaTemplateEngine(TemplateEngine):
     __config__: TemplateConfig      = TemplateConfig(path="", debug=True)
@@ -117,10 +179,13 @@ class JinjaTemplateEngine(TemplateEngine):
 
     __jinja__: JinjaConfig
     __sitemap__: SitemapIndexConfig
+    __head__: HeadConfig
+    __static__: str
 
-    def __init__(self, jinja: JinjaConfig, sitemap: SitemapIndexConfig) -> None:
+    def __init__(self, jinja: JinjaConfig, sitemap: SitemapIndexConfig, head: HeadConfig) -> None:
         self.__jinja__          = jinja
         self.__sitemap__        = sitemap
+        self.__head__           = head
 
     def Init(self, config: TemplateConfig) -> None:
         if self.__configed__: return
@@ -136,7 +201,7 @@ class JinjaTemplateEngine(TemplateEngine):
         contacttemp: Template       = env.get_template(self.__jinja__.contact)
         donatetemp: Template        = env.get_template(self.__jinja__.donate)
 
-        jsondata = JinjaJSON(
+        ORIGIN = JinjaJSON(
             data = DataJSON(
                 nav         = NavJSON(data=datapage.nav),
                 foot        = FootJSON(data=datapage.foot),
@@ -150,9 +215,30 @@ class JinjaTemplateEngine(TemplateEngine):
                 landing     = self.__sitemap__.landing,
                 product     = self.__sitemap__.product,
                 contact     = self.__sitemap__.contact,
-                donate      = self.__sitemap__.donate,
+                donate      = self.__sitemap__.donate
+            ),
+            head = HeadJSON(
+                langcode    = self.__head__.langcode,
+
+                author      = self.__head__.author,
+                email       = self.__head__.email,
+
+                icon16      = RemotePNGIconJSON(href=self.__head__.icon16.href, size=self.__head__.icon16.size),
+                icon32      = RemotePNGIconJSON(href=self.__head__.icon32.href, size=self.__head__.icon32.size),
+                icon64      = RemotePNGIconJSON(href=self.__head__.icon64.href, size=self.__head__.icon64.size),
+                icon128     = RemotePNGIconJSON(href=self.__head__.icon128.href, size=self.__head__.icon128.size),
+                icon256     = RemotePNGIconJSON(href=self.__head__.icon256.href, size=self.__head__.icon256.size),
+
+                opengraph   = RemotePNGJSON(
+                    href        = self.__head__.opengraph.href,
+                    width       = self.__head__.opengraph.width,
+                    height      = self.__head__.opengraph.height,
+                    alt         = self.__head__.opengraph.alt
+                )
             )
         )
+
+        jsondata = deepcopy(ORIGIN)
 
         landinghtml: str    = landingtemp.render(asdict(jsondata))
         producthtml: str    = producttemp.render(asdict(jsondata))
@@ -206,29 +292,13 @@ class JinjaTemplateEngine(TemplateEngine):
                 case _:
                     raise Exception(f"star-filter data of datapage at JinjaTemplateEngine.Run() can't be {item.filter.star}")
 
-            jsondata = JinjaJSON(
-                data = DataJSON(
-                    nav         = NavJSON(data=datapage.nav),
-                    foot        = FootJSON(data=datapage.foot),
-                    landing     = LandingJSON(index=datapage.landing.index),
-                    product     = ProductJSON(
-                        index       = item,
-                        filter      = ProductFilterJSON(
-                            level       = hreflevel,
-                            series      = hrefseries,
-                            star        = hrefstar
-                        )
-                    ),
-                    contact     = ContactJSON(index=datapage.contact.index),
-                    donate      = DonateJSON(index=datapage.donate.index)
-                ),
-                sitemap = SitemapJSON(
-                    root        = self.__sitemap__.root,
-                    landing     = self.__sitemap__.landing,
-                    product     = self.__sitemap__.product,
-                    contact     = self.__sitemap__.contact,
-                    donate      = self.__sitemap__.donate,
-                )
+            jsondata = deepcopy(ORIGIN)
+
+            jsondata.data.product.index = item
+            jsondata.data.product.filter = ProductFilterJSON(
+                level       = hreflevel,
+                series      = hrefseries,
+                star        = hrefstar
             )
 
             producthtmls.append(
